450a0faf859dd412584ffb2fd466b890aad56dd0
Dionkeldei <dionkeldei@gmail.com>
2021-03-30 19:26:04 -0600

    result array

--- index.js
+++ index.js
@@ -6,16 +6,16 @@ if (process.argv.length < 3) {
   console.log('Usage: node ' + process.argv[1] + ' FILENAME');
   process.exit(1);
 }
+
+results = [];
 // Read the file and print its contents.
 var fs = require('fs')
   , filename = process.argv[2];
 fs.readFile(filename, 'utf8', function(err, data) {
   if (err) throw err;
   console.log('Leyendo: ' + filename);
-
   data = docHelper.clean(data);
-  sortable = coincidences.get(data);
-
-  console.log(sortable);
-  console.log('Palabras usadas: '+sortable.length);
+  results = coincidences.get(data, results);
+  console.log(results);
+  console.log('Palabras usadas: '+results.length);
 });
