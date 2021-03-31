0dbbeeb1e59f32da9a94ed4144b518d2945db3d9
Dionkeldei <dionkeldei@gmail.com>
2021-03-31 14:35:06 -0600

    index cleaning

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
-
 const docHelper = require("./libraries/docHelper");
-
 
 // Make sure we got a filename on the command line.
 if (process.argv.length < 3) {
   console.log('Usage: node ' + process.argv[1] + ' FILENAME');
   process.exit(1);
 }
-
-results = [];
 
 var files = []
 var filename = process.argv[2];
@@ -30,5 +26,3 @@ if(isFolder){
   filename = filename.replace('.txt','.pdf')
   docHelper.countWords(files, filename)
 }
-
-// Read the file and print its contents.
